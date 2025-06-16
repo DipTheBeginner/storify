a3622c888cf2dd11fb929994e3929eb0f4af8543
DipTheBeginner <dipthebeginner@gmail.com>
2025-06-16 21:05:41 +0530

    desigining account

--- apps/web/app/account/[userId]/page.tsx
+++ apps/web/app/account/[userId]/page.tsx
@@ -5,6 +5,8 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useEffect } from "react";
 import FollowStats from "src/components/FollowStats";
+import NavBar from "src/components/NavBar";
+import HomeNavBar from "src/components/navbars/HomeNavBar";
 import StoryCard from "src/components/StoryCard";
 import { useAllStoryStore } from "src/zustand/stories/allStories";
 import { useUserStoryStore } from "src/zustand/stories/userStories";
@@ -20,11 +22,11 @@ function toPascalCase(name: string) {
 
 export default function AccountPage() {
   const { data: session, status } = useSession();
-  const{userStories,setUserStories}=useUserStoryStore
+  const { userStories, setUserStories } = useUserStoryStore
 
   const token = session?.user.token
 
-  console.log("Token is ",token)
+  console.log("Token is ", token)
 
   async function fetchMyStories() {
     if (!session?.user.id) {
@@ -36,9 +38,9 @@ export default function AccountPage() {
           'Authorization': `Bearer ${token}`
         }
       })
-      console.log("User id in acoount is ",session.user.id)
-      setUserStories(response.data.data)  
-      console.log("User story are",response.data)
+      console.log("User id in acoount is ", session.user.id)
+      setUserStories(response.data.data)
+      console.log("User story are", response.data)
 
     } catch (error) {
 
@@ -62,37 +64,33 @@ export default function AccountPage() {
 
   return (
 
-    <div className="bg-gray-200 min-h-screen relative flex flex-col items-center">
-
-      <div className="absolute top-0 left-0 w-full h-32 bg-white rounded-b-[80%]" />
-
-      <div className="mt-40 w-full flex items-center gap-6 bg-amber-800 h-50">
-        <div className="flex flex-row items-center gap-10 bg-yellow-400 ml-80">
-
-          <div className="bg-green-700">
-
-
-            {session.user.image && (
-              <Image
-                src={session.user.image}
-                alt="User profile"
-                width={200}
-                height={200}
-                className="rounded-full border border-gray-300 object-cover"
-              />
-            )}
-          </div>
-          <h1 className="text-2xl font-bold text-gray-800 bg-red-400 -mt-20 ">
+    <div>
+      <HomeNavBar />
+      <div className="bg-neutral-200 min-h-screen py-8 px-12">
+        {/* Profile Section */}
+        <div className="flex items-center gap-6 p-20 w-fit ml-40">
+          <Image
+            src={session.user.image!}
+            height={80}
+            width={80}
+            alt="user-profile"
+            className="rounded-full border border-gray-300 object-cover"
+          />
+          <span className="text-xl font-semibold text-gray-800">
             {toPascalCase(session.user.name || "")}
-          </h1>
+          </span>
         </div>
+        <div className="flex flex-row gap-10  w-fit ml-80 -mt-10">
+          <span className="text-lg ">
+            Home
+          </span>
+          <span className="text-lg">
+            About
+          </span>
+          
+        </div>
+        <div className="h-[1px] bg-gray-400 w-full mt-6" />
       </div>
-      <div className="font-semibold text-2xl bg-amber-500 w-full flex">
-        <span className="bg-red-500 ml-120">
-          My Posts
-        </span>
-      </div>
-
     </div>
   );
 }
