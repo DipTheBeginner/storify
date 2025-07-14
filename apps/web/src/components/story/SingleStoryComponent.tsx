cd1ff507b02b40b44570d4ceefd15ae9bfcf94c4
DipTheBeginner <dipthebeginner@gmail.com>
2025-07-14 21:35:03 +0530

    opening account from card

--- apps/web/src/components/story/SingleStoryComponent.tsx
+++ apps/web/src/components/story/SingleStoryComponent.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { MouseEvent } from "react";
 import { StoryType } from "src/types/types";
 
 interface SingleStoryComponentProps {
@@ -14,6 +16,13 @@ function toPascalCase(name: string) {
 }
 
 export default function SingleStoryComponent({ story }: SingleStoryComponentProps) {
+
+    const router=useRouter();
+     function openAccountHandler(e:MouseEvent<HTMLDivElement>){
+            e.stopPropagation();
+            console.log("small div also clicked");
+            router.push(`/@${story.author.email.split("@")[0]}`)
+        }
     return (
         <div className="w-[40%] mx-auto p-6 flex flex-col gap-4  ">
             {/* Top section: Title and Author Info */}
@@ -21,16 +30,19 @@ export default function SingleStoryComponent({ story }: SingleStoryComponentProp
                 <h1 className="text-3xl font-extrabold text-neutral-800">{story.title}</h1>
 
                 <div className="flex items-center gap-3">
-                    <Image
-                        src={story.author.image!}
-                        alt={`${story.author.name}'s profile`}
-                        width={24}
-                        height={24}
-                        className="rounded-full"
-                    />
-                    <span className="font-semibold text-sm text-neutral-800">
-                        {toPascalCase(story.author.name)}
-                    </span>
+                    <div className="" onClick={openAccountHandler}>
+
+                        <Image
+                            src={story.author.image!}
+                            alt={`${story.author.name}'s profile`}
+                            width={24}
+                            height={24}
+                            className="rounded-full"
+                        />
+                        <span className="font-semibold text-sm text-neutral-800">
+                            {toPascalCase(story.author.name)}
+                        </span>
+                    </div>
                     <span className="text-sm text-gray-400">
                         {new Date(story.createdAt).toLocaleDateString("en-US", {
                             year: "numeric",
@@ -47,7 +59,7 @@ export default function SingleStoryComponent({ story }: SingleStoryComponentProp
 
 
                 <div className="">
-                     storylikes
+                    storylikes
 
                 </div>
 
@@ -63,7 +75,7 @@ export default function SingleStoryComponent({ story }: SingleStoryComponentProp
                 </div>
 
                 <div>
-                    
+
                 </div>
             </div>
 
